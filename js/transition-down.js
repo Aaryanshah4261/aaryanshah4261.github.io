5015b3077b695d0008938a6d8efe0b0a2e0260a6
Aaryanshah4261 <aaryanshah4261@gmail.com>
2025-02-06 22:40:51 -0500

    Update transition-down.js

--- js/transition-down.js
+++ js/transition-down.js
@@ -8,3 +8,22 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("page-content").style.display = "block";
 }
+
+// JavaScript to handle modal popup
+function openModal(projectId) {
+  document.getElementById(projectId + '-modal').style.display = "block";
+}
+
+function closeModal(projectId) {
+  document.getElementById(projectId + '-modal').style.display = "none";
+}
+
+// Close the modal when the user clicks anywhere outside of it
+window.onclick = function(event) {
+  const modals = document.getElementsByClassName('modal');
+  for (let i = 0; i < modals.length; i++) {
+    if (event.target == modals[i]) {
+      modals[i].style.display = "none";
+    }
+  }
+}
